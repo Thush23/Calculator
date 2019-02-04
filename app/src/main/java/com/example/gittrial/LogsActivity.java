4f6286c139c9248c0d0da9d21ee690d1bb0f02b4
Thush <Thush@live.co.uk>
2019-02-04 10:50:11 +0000

    Finalised

---
+++ app/src/main/java/com/example/gittrial/LogsActivity.java
@@ -0,0 +1,28 @@
+package com.example.gittrial;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class LogsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+
+        TextView tvLogs = findViewById(R.id.tvLogs);
+
+        ArrayList<String> logs =
+                getIntent().getStringArrayListExtra("LogsResult");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < logs.size(); i++) {
+        stringBuilder.append(logs.get(i));
+        stringBuilder.append("\n");
+        }
+
+        tvLogs.setText(stringBuilder.toString());
+    }
+}
--- app/src/main/java/com/example/gittrial/MainActivity.java
+++ app/src/main/java/com/example/gittrial/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.gittrial;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,24 +8,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
     EditText etno1;
     EditText etno2;
     Button btnadd;
+    Button btnsub;
+    Button btndiv;
+    Button btnmul;
     TextView tvresult;
+    Button btnshowlogs;
+
+    private List<String> log = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         String message = "Hello";
 
         etno1 = findViewById(R.id.etno1);
         etno2 = findViewById(R.id.etno2);
         btnadd = findViewById(R.id.btnadd);
+        btnsub = findViewById(R.id.btnsub);
+        btndiv = findViewById(R.id.btndiv);
+        btnmul = findViewById(R.id.btnmul);
+        btnshowlogs = findViewById(R.id.btnshowlogs);
         tvresult = findViewById(R.id.tvresult);
 
         btnadd.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +45,48 @@ public class MainActivity extends AppCompatActivity {
                 String result = add(etno1.getText().toString(),
                         etno2.getText().toString());
                 tvresult.setText(result);
+                log.add("Result of Addition: "+result);
+            }
+        });
+
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = sub(etno1.getText().toString(),
+                        etno2.getText().toString());
+                tvresult.setText(result);
+                log.add("Result of Subtract: "+result);
+            }
+        });
+
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = div(etno1.getText().toString(),
+                        etno2.getText().toString());
+                tvresult.setText(result);
+                log.add("Result of Divide: "+result);
+            }
+        });
+
+        btnmul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = mul(etno1.getText().toString(),
+                        etno2.getText().toString());
+                tvresult.setText(result);
+                log.add("Result of Multiply: "+result);
+            }
+        });
+
+        btnshowlogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent
+                        (MainActivity.this, LogsActivity.class);
+                intent.putStringArrayListExtra("LogsResult",
+                        (ArrayList<String>)log);
+                startActivity(intent);
             }
         });
 
@@ -47,6 +100,42 @@ public class MainActivity extends AppCompatActivity {
         int a = Integer.parseInt(numberone);
         int b = Integer.parseInt(numbertwo);
         int result = a + b;
+        return Integer.toString(result);
+    }
+
+    private String sub(String numberone, String numbertwo){
+        if (numberone.equals("")|numbertwo.isEmpty()){
+            Toast.makeText(this, "Please enter a valid numer",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int a = Integer.parseInt(numberone);
+        int b = Integer.parseInt(numbertwo);
+        int result = a - b;
+        return Integer.toString(result);
+    }
+
+    private String div(String numberone, String numbertwo){
+        if (numberone.equals("")|numbertwo.isEmpty()){
+            Toast.makeText(this, "Please enter a valid numer",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int a = Integer.parseInt(numberone);
+        int b = Integer.parseInt(numbertwo);
+        int result = a / b;
+        return Integer.toString(result);
+    }
+
+    private String mul(String numberone, String numbertwo){
+        if (numberone.equals("")|numbertwo.isEmpty()){
+            Toast.makeText(this, "Please enter a valid numer",
+                    Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        int a = Integer.parseInt(numberone);
+        int b = Integer.parseInt(numbertwo);
+        int result = a * b;
         return Integer.toString(result);
     }
 
